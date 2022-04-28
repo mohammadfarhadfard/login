147fda8204c22d7adf3a009a8e233a1c02aba6fa
Hamid Teimouri <hamidteimouri@outlook.com>
2022-04-28 09:00:32 +0430

    bug fixed

--- index.php
+++ index.php
@@ -1,28 +1,28 @@
 <!DOCTYPE html>
-<html lang="ir">
+<html lang="fa">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="styles/style.css">
     <!-- <link rel="stylesheet" href="font-awesome/css/font-awesome.min.css"> -->
-    
+
     <title>login and sign up form </title>
 </head>
 <body>
-  <form action="save.php" method="post">
-      
+<form action="save.php" method="post">
+
     <img src="./icon/login_icon_137429.png" alt="icon">
     <div class="input-group">
-       <input type="text" id="user-name" name="user_1" required autocomplete="off">
-       <label for="user-name">user name</label>
+        <input type="text" id="user-name" name="user_1" required autocomplete="off">
+        <label for="user-name">user name</label>
     </div>
     <div class="input-group">
         <input type="password" id="user-pass" name="pass_1" required autocomplete="off">
         <label for="user-pass">password</label>
     </div>
     <input type="submit" id="submit" value="login">
-   </form>
-   <script src="js/formjs.js"></script>
- </body>
+</form>
+<script src="js/formjs.js"></script>
+</body>
 </html> 
--- save.php
+++ save.php
@@ -1,32 +1,22 @@
-<html>
-<head>
-<meta http-equiv="content-type" content="text/html" charset="utf-8">
-<title>Data storage</title>
-</head>
-<body>
-    
 <?php
-$Hostname = "localhost"
-$username = "root"
-$password = ""
-$dbname = "test"
+session_start();
+$hostname = "localhost";
+$username = "root";
+$password = "";
+$dbname = "test1";
 
 $username = $_POST['user_1'];
-$password = $_POST['pass_1']
+$password = $_POST['pass_1'];
 
-$conn = new mysqli ($Hostname,$username, $password, $dbname ) ;
-if ($conn->connect-error) {
-    die("connection failed: " . $conn->connect_error) ;
+$conn = new mysqli ($hostname, $username, $password, $dbname);
+if ($conn->connect_error) {
+    die("connection failed: " . $conn->connect_error);
 }
-$conn->set_charset("utf8") ;
-$sql = "INSERT INTO `form1` (`username`, `password`) VALUES ('$username' , '$password' ) " ;
-if ($conn->query($sql) ---TRUE) {
-echo "done,ok"
-}else{
-echo "error"
+$conn->set_charset("utf8");
+$sql = "INSERT INTO `form1` (`username`, `password`) VALUES ('$username' , '$password' ) ";
+if ($conn->query($sql)) {
+    echo "done,ok";
+} else {
+    die("failed to insert : " . $conn->error);
 }
-$conn->close() ;
-?>
-</body>
-
-</html>
+$conn->close();
